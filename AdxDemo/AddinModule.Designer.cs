ee1c7b2a90bef0a6aaed25445ccc84ecbd9e979e
Garfield Huang <33893841+garfiedhuang@users.noreply.github.com>
2019-06-14 00:06:59 +0800

    Add files via upload

---
+++ AdxDemo/AddinModule.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace AdxDemo
+{
+    partial class AddinModule
+    {
+        /// <summary>
+        /// Required by designer
+        /// </summary>
+        private System.ComponentModel.IContainer components;
+ 
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+        /// <summary>
+        /// Required by designer support - do not modify
+        /// the following method
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            //
+            // AddinModule
+            //
+            this.AddinName = "AdxDemo";
+            
+            this.SupportedApps = ((AddinExpress.MSO.ADXOfficeHostApp)(
+				AddinExpress.MSO.ADXOfficeHostApp.ohaExcel |
+				AddinExpress.MSO.ADXOfficeHostApp.ohaWord |
+				AddinExpress.MSO.ADXOfficeHostApp.ohaPowerPoint 
+				));
+        }
+        #endregion
+    }
+}
+
---
+++ AdxDemo/AddinModule.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Windows.Forms;
+using AddinExpress.MSO;
+using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Dots_InmengTools;
+
+namespace AdxDemo
+{
+    /// <summary>
+    ///   Add-in Express Add-in Module
+    /// </summary>
+    [GuidAttribute("47BD5B83-4481-4759-8095-89C4C52161B9"), ProgId("AdxDemo.AddinModule")]
+    public partial class AddinModule : AddinExpress.MSO.ADXAddinModule
+    {
+        public AddinModule()
+        {
+            Application.EnableVisualStyles();
+            InitializeComponent();
+            // Please add any initialization code to the AddinInitialize event handler
+        }
+ 
+        #region Add-in Express automatic code
+ 
+        // Required by Add-in Express - do not modify
+        // the methods within this region
+ 
+        public override System.ComponentModel.IContainer GetContainer()
+        {
+            if (components == null)
+                components = new System.ComponentModel.Container();
+            return components;
+        }
+ 
+        [ComRegisterFunctionAttribute]
+        public static void AddinRegister(Type t)
+        {
+            AddinExpress.MSO.ADXAddinModule.ADXRegister(t);
+        }
+ 
+        [ComUnregisterFunctionAttribute]
+        public static void AddinUnregister(Type t)
+        {
+            AddinExpress.MSO.ADXAddinModule.ADXUnregister(t);
+        }
+ 
+        public override void UninstallControls()
+        {
+            base.UninstallControls();
+        }
+
+        #endregion
+
+        public static new AddinModule CurrentInstance 
+        {
+            get
+            {
+                return AddinExpress.MSO.ADXAddinModule.CurrentInstance as AddinModule;
+            }
+        }
+
+        public Excel._Application ExcelApp
+        {
+            get
+            {
+                return (HostApplication as Excel._Application);
+            }
+        }
+
+        public Word._Application WordApp
+        {
+            get
+            {
+                return (HostApplication as Word._Application);
+            }
+        }
+
+        public PowerPoint._Application PowerPointApp
+        {
+            get
+            {
+                return (HostApplication as PowerPoint._Application);
+            }
+        }
+
+
+        #region my logic
+
+        private DotsCovAndPrintTool dcpt = new DotsCovAndPrintTool();
+
+
+        #endregion
+
+    }
+}
+
